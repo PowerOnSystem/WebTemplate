883334d90cdc0e2b7235a33e578ce673703bd98c
Lucas Sosa <sosalucas87@gmail.com>
2017-06-16 15:44:14 -0300

    Important changes

--- config/console.php
+++ config/console.php
@@ -18,7 +18,7 @@
  */
 
 namespace PowerOn\Console;
-
+use Composer\Script\Event;
 /**
  * Console configura todo el proyecto luego de instalarlo
  * @author Lucas Sosa
@@ -28,13 +28,54 @@ class Console {
     /**
      * Crea la clave única de la aplicación
      */
-    public static function postInstall() {
+    public static function postInstall(Event $event) {
         $dir = dirname(dirname(__FILE__));
+        $dir_explode = explode(DIRECTORY_SEPARATOR, $dir);
+        
+        $app = end($dir_explode);
+        
+        $io = $event->getIO();
+        
+        $io->write('Generando clave de seguridad unica.');
         $keyfile = $dir . DIRECTORY_SEPARATOR . 'appkey.txt';
         
         $file = fopen($keyfile, 'w');
         fwrite($file, bin2hex(random_bytes(96)));
         fclose($file);
         
+        $io->write('Configurando aplicacion.');
+        
+        self::createJsonFile($dir, $app, $io);
+        
+        
+    }
+    /**
+     * Crea el archivo composer
+     * @param string $dir
+     * @param string $app Nombre de la aplicacion
+     * @param IO $io
+     */
+    public static function createJsonFile($dir, $app, $io) {
+        $composer_file = $dir . DIRECTORY_SEPARATOR . 'composer.json';
+        $composer_lock = $dir . DIRECTORY_SEPARATOR . 'composer.lock';
+        if ( file_exists($composer_file) ) {
+            unlink($composer_file);
+        }
+        if (file_exists($composer_lock) ) {
+            unlink($composer_lock);
+        }
+        
+        $file = fopen($dir . DIRECTORY_SEPARATOR . 'composer.json', 'w');
+        fwrite($file, 
+            '{' . PHP_EOL .
+                '\t"name": "poweronsystem/' . $app . '",' . PHP_EOL .
+                '\t"type": "library",' . PHP_EOL .
+                '\t"require": {' . PHP_EOL .
+                    '"\t\tpoweronsystem/webframework": "^0.1.0"' . PHP_EOL .
+                '\t}' . PHP_EOL .
+            '}');
+        fclose($file);
+        
+        $io->write('El archivo composer.json fue generado correctamente.');
     }
 }
--- webroot/index.php
+++ webroot/index.php
@@ -4,7 +4,7 @@
  */
 session_start();
 
-define('ROOT', dirname(dirname(dirname(__FILE__))));
+define('ROOT', dirname(dirname(__FILE__)));
 define('DS', DIRECTORY_SEPARATOR);
 define('DEV_ENVIRONMENT', FALSE);
 
